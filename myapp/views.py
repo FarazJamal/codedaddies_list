b6d2f39b909b49f4e1fd3bf0dc57204397aabba2
FarazJamal <mrfarazjamal@gmail.com>
2023-12-29 20:17:44 +0500

    cg

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+import requests
+
+BASE_CRAIGLIST_URL = 'https://newyork.craigslist.org/search/bbb?query={}'
 
 # Create your views here.
 
@@ -8,6 +11,9 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    response = requests.get('https://newyork.craigslist.org/search/bbb?query=python#search=1~thumb~0~0')
+    data = response.text
+    print(data)
     for_frontend = {
         'search' : search
     }
